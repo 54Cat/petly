37e9a737c1630f5c274421109625781520569208
EnVy <envy7321@gmail.com>
2023-02-01 11:51:57 +0200

    Установил библиотеку с картинками

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import { Route, Routes } from 'react-router-dom';
 import SharedLayout from './components/SharedLayout/SharedLayout';
 import { RegisterPage } from 'pages/Register/RegisterPage';
 import { NewsPage } from 'pages/News/NewsPage';
+import { LoginPage } from 'pages/Login/LoginPage';
 
 const App = () => {
-
     return (
         <>
             <Routes>
                 <Route path="/" element={<SharedLayout />}>
                     <Route path="register" element={<RegisterPage />} />
+                    <Route path="login" element={<LoginPage />} />
                     <Route path="news" element={<NewsPage />} />
                 </Route>
             </Routes>
