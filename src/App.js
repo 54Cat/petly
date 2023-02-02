f1572dc647ec05515a60a999cc160b1d77aefee0
Maksym Kushnirov <maksymkushnirov@gmail.com>
2023-02-02 20:19:15 +0200

    file structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import SharedLayout from './components/SharedLayout/SharedLayout';
+// import { Header } from './components/Header/Header';
+// import { Logo } from './components/Header/Logo';
+// import { Navigation } from './components/Header/Navigation';
+// import { Auth } from './components/Header/Auth';
+// import { MenuAuth } from './components/Header/MenuBurger/MenuAuth';
+// import { MenuHeader } from './components/Header/MenuBurger/MenuHeader';
+// import { MenuPages } from './components/Header/MenuBurger/MenuPages';
 import { RegisterPage } from 'pages/Register/RegisterPage';
 import { NewsPage } from 'pages/News/NewsPage';
 import { FriendsPage } from 'pages/Friends/FriendsPage';
@@ -9,9 +16,16 @@ const App = () => {
         <>
             <Routes>
                 <Route path="/" element={<SharedLayout />}>
-                    <Route path="register" element={<RegisterPage />} />
-                    <Route path="news" element={<NewsPage />} />
-                    <Route path="friends" element={<FriendsPage />} />
+                {/* <Route path="header" element={<Header />} />
+                <Route path="header" element={<Logo />} />
+                <Route path="header" element={<Navigation />} />
+                <Route path="header" element={<Auth />} />
+                <Route path="header/menuburger/" element={<MenuAuth />} />
+                <Route path="header/menuburger/" element={<MenuHeader />} />
+                <Route path="header/menuburger/" element={<MenuPages />} /> */}
+                <Route path="register" element={<RegisterPage />} />
+                <Route path="news" element={<NewsPage />} />
+                <Route path="friends" element={<FriendsPage />} />
                 </Route>
             </Routes>
         </>
