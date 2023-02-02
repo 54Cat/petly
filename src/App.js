90e60f7ea5eaeb28f69617593a8ea905a11ee25f
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2023-02-02 09:49:11 +0200

    add friendsPage

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import SharedLayout from './components/SharedLayout/SharedLayout';
 import { RegisterPage } from 'pages/Register/RegisterPage';
 import { NewsPage } from 'pages/News/NewsPage';
+import { FriendsPage } from 'pages/Friends/FriendsPage';
 
 const App = () => {
-
     return (
         <>
             <Routes>
                 <Route path="/" element={<SharedLayout />}>
                     <Route path="register" element={<RegisterPage />} />
                     <Route path="news" element={<NewsPage />} />
+                    <Route path="friends" element={<FriendsPage />} />
                 </Route>
             </Routes>
         </>
---
+++ src/pages/Friends/FriendsPage.jsx
@@ -0,0 +1,13 @@
+
+import { PageSection } from 'components/Utils/Styles/basicStyle'
+import { Title } from 'components/Utils/Styles/basicStyle'
+import { CardsList } from 'components/CardsList/CardsList'
+
+
+
+export const FriendsPage = () => {
+    return <PageSection>
+        <Title>Our friend</Title>
+        {/* <CardsList cardsArray={news} CardsItem={NewsCard} /> */}
+    </PageSection>
+}
