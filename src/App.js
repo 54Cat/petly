ed5cfd7fe6673cda30c9049380a5c8dfc47fae30
Maksym Kushnirov <maksymkushnirov@gmail.com>
2023-02-02 19:20:24 +0200

    file structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import SharedLayout from './components/SharedLayout/SharedLayout';
+import { Header } from 'components/Header';
+import { Logo } from 'components/Logo';
+import { Navigation } from 'components/Navigation';
+import { Auth } from 'components/Auth';
+import { MenuAuth } from 'components/MenuAuth';
+import { MenuHeader } from 'components/MenuHeader';
+import { MenuPages } from 'components/MenuPages';
 import { RegisterPage } from 'pages/Register/RegisterPage';
 import { NewsPage } from 'pages/News/NewsPage';
 
@@ -9,6 +16,13 @@ const App = () => {
         <>
             <Routes>
                 <Route path="/" element={<SharedLayout />}>
+                    <Route path="register" element={<Header />} />
+                    <Route path="register" element={<Logo />} />
+                    <Route path="register" element={<Navigation />} />
+                    <Route path="register" element={<Auth />} />
+                    <Route path="register" element={<MenuAuth />} />
+                    <Route path="register" element={<MenuHeader />} />
+                    <Route path="register" element={<MenuPages />} />
                     <Route path="register" element={<RegisterPage />} />
                     <Route path="news" element={<NewsPage />} />
                 </Route>
