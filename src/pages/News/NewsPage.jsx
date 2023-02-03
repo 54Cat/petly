00a84badb9cb3d23eddf930d290ade858c236a72
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2023-02-03 09:48:49 +0200

    add fetch news

--- src/pages/News/NewsPage.jsx
+++ src/pages/News/NewsPage.jsx
@@ -1,26 +1,28 @@
-import { useState} from 'react'
+import { useState } from 'react'
+import { fetchNews } from 'components/Utils/axios/fetchNews'
 import { PageSection } from 'components/Utils/Styles/basicStyle'
 import { Title } from 'components/Utils/Styles/basicStyle'
 import { SearchBar } from 'components/SearchBar/SearchBar'
 import { CardsList } from 'components/CardsList/CardsList'
 import NewsCard from 'components/NewsCard/NewsCard'
-import lastNews from 'data/news.json'
 
 
 export const NewsPage = () => {
     const [filter, setFilter] = useState('')
-    const [news, setNews] = useState(lastNews)
-
-    
-    
-
+    const [news, setNews] = useState([])
     const onFilterChange = e => {
     setFilter(e.currentTarget.value);
     };
 
-    const handleSubmit = e => {
+    const handleSubmit = async e => {
         e.preventDefault();
-        setNews(lastNews);
+        try {
+            const results = await fetchNews(filter);
+            setNews(results);
+        } catch (e) {
+            console.log(e.message)
+        }
+        
     };
     
 
