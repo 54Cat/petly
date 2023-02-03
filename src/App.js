d77d637c1e8a0b0d5855c46519bdcb4d31473d48
chekirko <victorche59@gmail.com>
2023-02-03 06:04:03 +0200

    add PetsList Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SharedLayout from './components/SharedLayout/SharedLayout';
 import { RegisterPage } from 'pages/Register/RegisterPage';
 import { NewsPage } from 'pages/News/NewsPage';
 import { FriendsPage } from 'pages/Friends/FriendsPage';
+import { UserPage } from 'pages/User/UserPage';
 
 const App = () => {
     return (
@@ -12,6 +13,7 @@ const App = () => {
                     <Route path="register" element={<RegisterPage />} />
                     <Route path="news" element={<NewsPage />} />
                     <Route path="friends" element={<FriendsPage />} />
+                    <Route path="user" element={<UserPage />} />
                 </Route>
             </Routes>
         </>
--- src/components/PetsList/PetsList.jsx
+++ src/components/PetsList/PetsList.jsx
@@ -1,25 +1,82 @@
-import PetsItem from "components/PetsItem/PetsItem";
+import {
+    Pets,
+    PetsCard,
+    PetsImg,
+    TextWrapper,
+    PetsName,
+    CardText,
+    CardTitle,
+    DelBtn,
+    StyledDeleteIcon,
+} from './PetsList.styled';
 
 const PetsList = function () {
-    const pets = [{
-        id: 7676, name: "Johnny", date: Date.now(), breed: "dog", text: "jhjhjhj", img: "jhjh"
-    },
-    {
-        id: 8787, name: "Jack", date: Date.now(), breed: "cat", text: "87878778", img: "jhjh"
-    }]
-    const isLoggedIn = true
+    const pets = [
+        {
+            id: 7676,
+            name: 'Johnny',
+            date: Date.now(),
+            breed: 'dog',
+            text: 'jhjhjhj',
+            img: 'jhjh',
+        },
+        {
+            id: 8787,
+            name: 'Jack',
+            date: Date.now(),
+            breed: 'cat',
+            text: 'jhjhhf овр оао овавро оваро овао овао оваро роваора оврао оваро вораорао воаро воар оваро овроав воаров овро овdjh dfhh djfh dhf jhfdh dfj djh djh dhj djh djh dh djh djfh hd dh djh d hdfj djf djh dj djh dfj dfj djh djh djh jdh djh dfh dfh dfh djh djh djh  dаро овро оровар аора варов р ао аорр аоа о  росар о о ао ',
+            img: 'fg',
+        },
+    ];
+    const isLoggedIn = true;
+
+    const onBtnDelete = () => {
+        console.log('click');
+        // dispatch(deleteContact(id));
+    };
 
     return (
         <>
-        {isLoggedIn && pets.map(({ id, name, date, breed, text, img }) => {
-            return <PetsItem key={id} id={id} name={name} date={date} breed={breed} text={text} img={img} />
-        })}            
-        
-      
-      {pets.length === 0 && (
-        <p>There`s no pets yet</p>
-      )}</>
-    )
-}
+            {isLoggedIn && (
+                <Pets>
+                    {pets.map(({ id, name, date, breed, text, img }) => {
+                        return (
+                            <PetsCard>
+                                <PetsImg
+                                    src="https://upload.wikimedia.org/wikipedia/commons/4/43/Ambersweet_oranges.jpg"
+                                    alt=""
+                                />
+                                <TextWrapper>
+                                    <PetsName>
+                                        <CardTitle>Name:</CardTitle> {name}{' '}
+                                        <DelBtn
+                                            type="button"
+                                            onClick={onBtnDelete}
+                                        >
+                                            <StyledDeleteIcon sx={{}} />
+                                        </DelBtn>
+                                    </PetsName>
+                                    <CardText>
+                                        <CardTitle>Date of birth:</CardTitle>{' '}
+                                        {date}
+                                    </CardText>
+                                    <CardText>
+                                        <CardTitle>Breed:</CardTitle> {breed}
+                                    </CardText>
+                                    <CardText>
+                                        <CardTitle>Comments:</CardTitle> {text}
+                                    </CardText>
+                                </TextWrapper>
+                            </PetsCard>
+                        );
+                    })}
+                </Pets>
+            )}
 
-export default PetsList
+            {pets.length === 0 && <p>There`s no pets yet</p>}
+        </>
+    );
+};
+
+export default PetsList;
