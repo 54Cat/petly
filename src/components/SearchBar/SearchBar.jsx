3f2504ebb4465a63232fd93a28d8a96cf0157483
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2023-02-03 15:31:06 +0200

    create notices page, list, nav and add styles

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,8 @@
 import SearchIcon from '@mui/icons-material/Search';
 import { SearchButton, SearchForm } from './SearchBarStyled'
 import { InputSearch  } from 'components/Utils/Styles/basicStyle'
-export const SearchBar = ({handleSubmit, filter, onFilterChange}) => {
-    return <SearchForm onSubmit={handleSubmit}>
+export const SearchBar = ({handleSubmit, filter, onFilterChange, styles}) => {
+    return <SearchForm style={styles} onSubmit={handleSubmit}>
             <InputSearch value={filter} onChange={onFilterChange} placeholder={'Search'}></InputSearch>
         <SearchButton type="submit" ><SearchIcon  sx={{ fontSize: 20 }} /></SearchButton>
         </SearchForm>
