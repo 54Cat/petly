b92f880cc1ad1f3a43c7988dab3bfc06527d14fa
Anton Clever <antvcl@gmail.com>
2023-02-02 21:58:48 -0700

    AuthNav component done

---
+++ src/components/AuthNav/AuthNav.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { AuthNavItem, NavBtnLink } from './AuthNav.styled';
+
+const AuthNav = () => {
+    return (
+        <>
+            <AuthNavItem>
+                <NavBtnLink
+                    to="/login"
+                    className={({ isActive }) => (isActive ? 'active' : '')}
+                >
+                    Login
+                </NavBtnLink>
+            </AuthNavItem>
+            <AuthNavItem>
+                <NavBtnLink
+                    to="/register"
+                    className={({ isActive }) => (isActive ? 'active' : '')}
+                >
+                    Registration
+                </NavBtnLink>
+            </AuthNavItem>
+        </>
+    );
+};
+
+export default AuthNav;
