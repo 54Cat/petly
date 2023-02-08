8e137ee24062966826a733c4ce05ba7aa4a04ec9
Kateryna Hrabanova <KaterynaHrabanova@gmail.com>
2023-02-08 08:47:14 +0200

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,16 @@
-import HeaderLogo from './HeaderLogo';
-import Navigations from './HeaderNavigations';
-import HeaderAuthNav from './HeaderAuthNav';
-import MenuBurger from './MenuMobile/MenuBurger';
+import Logo from './HeaderLogo';
+// import Navigations from './HeaderNavigations';
+// import HeaderAuthNav from './HeaderAuthNav';
+// import MenuBurger from './MenuMobile/MenuBurger';
 
 const Header = () => {
   return (
-    <>
-        <Header>
-            <HeaderLogo />
-            <Navigations />
-            <HeaderAuthNav />
-            <MenuBurger />
-        </ Header>
-    </>
+    <header>
+      <Logo />
+      {/* <Navigations /> */}
+      {/* <HeaderAuthNav /> */}
+      {/* <MenuBurger /> */}
+  </ header>
   );
 };
 
