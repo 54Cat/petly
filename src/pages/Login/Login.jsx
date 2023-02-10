fdc13b9abde13d535f4d9f48b3ce563d23f72147
EnVy <envy7321@gmail.com>
2023-02-10 09:50:46 +0200

    Fixed styles + loader login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,7 +2,7 @@ import { Formik, Form, ErrorMessage } from 'formik';
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 import { loginUser } from 'redux/Auth/AuthSlice';
 import { loginValidationSchema } from '../Login/schemaValidation/SchemaValid';
 
