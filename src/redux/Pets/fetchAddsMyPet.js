afcc2f0e6c35a52a1a152154d15bfca2323b34b2
EnVy <envy7321@gmail.com>
2023-02-11 22:57:16 +0200

    ADD

--- src/redux/Pets/fetchAddsMyPet.js
+++ src/redux/Pets/fetchAddsMyPet.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
-
 const instance = axios.create({
-    baseURL: "https://petly-backend-23cb.onrender.com"
+    baseURL: 'https://petly-backend-23cb.onrender.com',
 });
 
-export const addMyPet = async (data) => {
+export const addMyPet = async data => {
     const response = await instance.post('user/pets', data);
     return response;
 };
