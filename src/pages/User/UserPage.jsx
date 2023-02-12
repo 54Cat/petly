507c915d452230f5051ada0cf880314721b6b8b7
VOLODYMYR BASHTANYUK <96777965+VolodymyrBashtanyuk@users.noreply.github.com>
2023-02-12 15:20:57 +0200

    test

--- src/pages/User/UserPage.jsx
+++ src/pages/User/UserPage.jsx
@@ -1,24 +1,16 @@
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { getUserOperation } from 'redux/User/userOperations';
-
 import UserData from 'components/UserData/UserData';
 import PetsData from 'components/PetsData/PetsData';
 import { Container, ButtonContainer } from './UserPageStyled';
 import ButtonAddsPet from 'components/ButtonAddsPet/ButtonAddsPet';
 
 const UserPage = () => {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(getUserOperation());
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [dispatch]);
-
     return (
         <Container>
-            <ButtonContainer><ButtonAddsPet /></ButtonContainer>
+            <ButtonContainer>
+                <ButtonAddsPet />
+            </ButtonContainer>
             <UserData />
-            <PetsData />   
+            <PetsData />
         </Container>
     );
 };
--- src/pages/User/UserPageStyled.jsx
+++ src/pages/User/UserPageStyled.jsx
@@ -1,37 +1,37 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-    position: relative;
     display: flex;
     flex-direction: column;
+    justify-content: center;
     align-items: center;
-    padding-top: 120px;
-    row-gap: 45px;
+    margin-top: 70px;
 
     @media (min-width: 768px) {
-    padding-top: 160px;
-    row-gap: 20px;
+        display: block;
+        margin-top: 88px;
     }
 
     @media (min-width: 1280px) {
         flex-direction: row;
         align-items: start;
-        padding-top: 125px;
-        row-gap: 0;
-        column-gap: 32px;
+        margin-top: 38px;
     }
 `;
 
 export const ButtonContainer = styled.div`
-position: absolute; 
-right: 20px; 
-top: 815px;
-@media (min-width: 768px) {
-    right: 32px;
-    top: 207px;
+    position: absolute;
+    right: 20px;
+    top: 815px;
+    @media (min-width: 768px) {
+        right: 32px;
+        top: 207px;
     }
     @media (min-width: 1280px) {
-    
-    top: 170px;
+        top: 170px;
     }
-`
+`;
+export const ErrorMessage = styled.p`
+    color: #ff0505;
+    font-size: 30px;
+`;
