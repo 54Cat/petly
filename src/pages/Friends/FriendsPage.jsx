0d446fd6f68d2f5d5701e744e209bbb72a1734fe
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2023-02-12 09:59:57 +0200

    add fetchFriedns

--- src/pages/Friends/FriendsPage.jsx
+++ src/pages/Friends/FriendsPage.jsx
@@ -1,16 +1,36 @@
 import { useState, useEffect } from 'react';
+import Notiflix from 'notiflix'
 import { PageSection } from 'components/Utils/Styles/basicStyle'
 import { Title } from 'components/Utils/Styles/basicStyle'
 import { CardsList } from 'components/CardsList/CardsList'
 import OurFriendsCard from 'components/OurFriendsCard/OurFriendsCard'
-import friends from 'data/sponsors.json'
 import { onTitleStyles, onListStyles } from 'components/Utils/helpers/friendsResizeStyles';
+import { fetchFriends } from 'redux/Friends/fetchFriends';
+import { CardsLoader } from 'components/Utils/Loader/Loader';
 
 const FriendsPage = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [friends, setFriends] = useState([])
   const [windowSize, setWindowSize] = useState([
     window.innerWidth,
     window.innerHeight,
   ]);
+
+  useEffect(() => {
+    onFirstRender()  
+  }, [])
+  
+  const onFirstRender = async () => {
+        try {
+            setIsLoading(true)
+            const results = await fetchFriends();
+            setFriends(results);
+        } catch (e) {
+            Notiflix.Notify.failure(e.message)
+        } finally {
+            setIsLoading(false)
+        }
+    }
 
   useEffect(() => {
     const handleWindowResize = () => {
@@ -25,8 +45,8 @@ const FriendsPage = () => {
   });
     
     return <PageSection>
-        <Title style={onTitleStyles(windowSize)}>Our friends</Title>
-        <CardsList styles={onListStyles(windowSize)}  cardsArray={friends} CardsItem={OurFriendsCard} />
+      <Title style={onTitleStyles(windowSize)}>Our friends</Title>
+      {isLoading ? <CardsLoader /> : <CardsList styles={onListStyles(windowSize)}  cardsArray={friends} CardsItem={OurFriendsCard} />}        
     </PageSection>
 }
 
