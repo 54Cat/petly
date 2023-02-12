1ed1219e82cd19c67ceb386e01ea8f0e3c73e4eb
EnVy <envy7321@gmail.com>
2023-02-12 10:45:59 +0200

    six name input

--- src/components/Modals/ModalAddsPet/steps/StepOne.jsx
+++ src/components/Modals/ModalAddsPet/steps/StepOne.jsx
@@ -18,14 +18,14 @@ import * as yup from 'yup';
 //import { DatePickerField } from 'components/Datapicker/Datapicker';
 const cyrillic = /[A-Za-z]/;
 const validationSchema = yup.object({
-    name: yup
+    names: yup
         .string()
         .min(2)
         .max(16)
         .required()
         .matches(cyrillic, 'only Latin letters'),
-    birthday: yup.string().required(),
-    breed: yup
+    birthdays: yup.string().required(),
+    breeds: yup
         .string()
         .min(2)
         .max(16)
@@ -64,39 +64,39 @@ const StepOne = props => {
                     <FormStyled>
                         <Title>Add pet</Title>
                         <LabelBox>
-                            <StyledLabel htmlFor="name">
+                            <StyledLabel htmlFor="names">
                                 Name pet
                                 <FieldStyled
                                     type="text"
-                                    name="name"
+                                    name="names"
                                     placeholder="Type name pet"
                                 />
                             </StyledLabel>
                         </LabelBox>
-                        <FormError name="name" />
+                        <FormError name="names" />
                         <LabelBox>
-                            <StyledLabel htmlFor="birthday">
+                            <StyledLabel htmlFor="birthdays">
                                 Date of birth
                                 <StyledPicker
                                     type="text"
-                                    name="birthday"
+                                    name="birthdays"
                                     placeholder="Type date of birth"
                                 />
                             </StyledLabel>
                         </LabelBox>
-                        <FormError name="birthday" />
+                        <FormError name="birthdays" />
 
                         <LabelBox>
-                            <StyledLabel htmlFor="breed">
+                            <StyledLabel htmlFor="breeds">
                                 Breed
                                 <FieldStyled
                                     type="text"
-                                    name="breed"
+                                    name="breeds"
                                     placeholder="Type breed"
                                 />
                             </StyledLabel>
                         </LabelBox>
-                        <FormError name="breed" />
+                        <FormError name="breeds" />
                         <FlexBox>
                             <NextBtn type="submit">Next</NextBtn>
                             <CancelBtn type="button" onClick={() => onClose()}>
