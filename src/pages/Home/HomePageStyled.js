9e4af9fd2a59b402afb251bbe9845950a64ba4a4
Kateryna Hrabanova <KaterynaHrabanova@gmail.com>
2023-02-25 23:26:12 +0200

    Update HomePageStyled.js

--- src/pages/Home/HomePageStyled.js
+++ src/pages/Home/HomePageStyled.js
@@ -13,60 +13,70 @@ import bg_inside_desktop from 'data/img_home_page/desktop/bg_inside_desktop.png'
 import bg_outside_desktop from 'data/img_home_page/desktop/bg_outside_desktop.png';
 
 export const Container = styled.div`
-  background: #fdf7f2;
+  background-color: #fdf7f2;
+  position: fixed;
+  top: 0;
+  left: 0;
   width: 100%;
+  height: 100%;
   padding-top: 74px;
   background-image: url(${bg_mobile});
   background-repeat: no-repeat;
-  background-position: bottom center;
-  background-size: 100% 455px;
+  background-position: center bottom 74px ;
+  background-size: 100% 67%;
 
   @media (min-width: 768px) {
     padding-top: 96px;
     background-image: url(${bg_tablet});
-    background-size: 100% 1100px;
+    background-position: center bottom 96px;
+    background-size: 100% 85%;
   }
       
   @media (min-width: 1280px) {
-    background-image: url(${hero_desktop}), url(${bg_inside_desktop}), url(${bg_outside_desktop}), url(${bg_heart_desktop}), url(${bg_point_desktop});
-    background-position: bottom right 16px, bottom left -10px, bottom right -50px, top 170px right 40%, bottom 250px left 520px;    
-    background-size: auto, 96% 350px, 100% 410px, auto, auto;
+    background-image: url(${bg_inside_desktop}), url(${bg_outside_desktop}), url(${bg_heart_desktop}), url(${bg_point_desktop});
+    background-position: bottom 96px left, bottom 96px right -52px, top 170px right 40%, bottom 296px left 38%;    
+    background-size: 96% 37%, 100% 46%, auto, auto;
   }
 `;
 
-export const Hero = styled.div`
-  height: 337px;
+export const Hero = styled.div`  
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 56%;
   background-image: url(${hero_mobile});
   background-repeat: no-repeat;
-  background-position: bottom center;
+  background-position: center bottom;
   background-size: contain;
 
   @media (min-width: 768px) {
-      height: 715px;
-      background-image: url(${hero_tablet});
+    height: 60%;
+    background-image: url(${hero_tablet});
   }
 
   @media (min-width: 1280px) {
-      background-image: none;
-      height: 420px;
+    height: 83%;
+    background-image: url(${hero_desktop});
+    background-position: right bottom;
   }
 `;
 
 export const Title = styled.p`
-  padding: 42px 0 58px 20px;
+  padding: 42px 0 0 20px;
   font-weight: 700;
   font-size: 32px;
   line-height: 44px;
   max-width: 280px;
 
   @media (min-width: 768px) {
-      padding: 68px 0 118px 32px;
-      font-size: 68px;
-      line-height: 100px;
-      max-width: 588px;
+    padding: 68px 0 0 32px;
+    font-size: 68px;
+    line-height: 100px;
+    max-width: 588px;
   }
 
   @media (min-width: 1280px) {
-      padding: 72px 0 0 16px;
+    padding: 72px 0 0 16px;
   }
 `;
